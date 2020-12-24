72f66ba61874b4eb2179575998649364721bd414
keskin <keskin.reactnative@gmail.com>
2020-12-25 01:04:55 +0300

    add header and body text

--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -5,17 +5,57 @@ import {
   View,
   ImageBackground,
   SafeAreaView,
+  Image,
+  Dimensions,
+  ActivityIndicator,
 } from "react-native";
+import {
+  useFonts,
+  Montserrat_700Bold,
+  Montserrat_600SemiBold,
+  Montserrat_800ExtraBold,
+} from "@expo-google-fonts/montserrat";
+
+const Width = Dimensions.get("window").width;
+const Height = Dimensions.get("window").height;
 
 const WelcomeScreen = () => {
-  return (
-    <View style={styles.container}>
-      <ImageBackground
-        source={require("../../assets/welcomebg.png")}
-        style={styles.background}
-      ></ImageBackground>
-    </View>
-  );
+  let [fontsLoaded] = useFonts({
+    Montserrat_700Bold,
+    Montserrat_600SemiBold,
+    Montserrat_800ExtraBold,
+  });
+
+  if (!fontsLoaded) {
+    return <ActivityIndicator />;
+  } else {
+    return (
+      <View style={styles.container}>
+        <ImageBackground
+          source={require("../../assets/welcomebg.png")}
+          style={styles.background}
+        >
+          <View>
+            <View style={styles.headercontainer}>
+              <Image source={require("../../assets/header.png")} />
+            </View>
+            <View style={styles.body}>
+              <View style={styles.bodytitlebox}>
+                <Text style={styles.bodytitletext}>Başlamadan Önce..</Text>
+              </View>
+              <View style={styles.bodybox}>
+                <Text style={styles.bodytext}>
+                  Evinizden en iyi radyo istasyonlarının keyfini çıkarın, hiçbir
+                  şeyi kaçırmayın
+                </Text>
+              </View>
+              <View style={styles.bodybutton}></View>
+            </View>
+          </View>
+        </ImageBackground>
+      </View>
+    );
+  }
 };
 
 const styles = StyleSheet.create({
@@ -30,6 +70,40 @@ const styles = StyleSheet.create({
     bottom: 0,
     backgroundColor: "#01012A",
   },
+  headercontainer: {
+    width: "100%",
+    height: Height / 4.28,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  body: {
+    width: 310,
+    alignSelf: "center",
+    height: 625,
+  },
+  bodytitlebox: {
+    width: 241,
+    height: 126,
+  },
+  bodytitletext: {
+    fontSize: 37,
+    fontWeight: "700",
+    lineHeight: 43,
+    fontFamily: "Montserrat_600SemiBold",
+    color: "white",
+  },
+  bodybox: {
+    width: 204,
+    height: 82,
+  },
+  bodytext: {
+    lineHeight: 19,
+    fontWeight: "400",
+    fontSize: 16,
+    color: "white",
+    textAlign: "left",
+  },
+  bodybutton: {},
 });
 
 export default WelcomeScreen;
